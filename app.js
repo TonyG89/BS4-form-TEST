bd086b8facf758888662bb93e70054884f3dbcc5
Anton Hryhoriev <a.gryhoryev@gmail.com>
2022-11-04 12:12:19 +0200

    done! only cleaning

--- app.js
+++ app.js
@@ -3,16 +3,42 @@ $("#sendForm").on("click", () => {
     const email = $("#email").val()
     const tel = $("#tel").val()
 
-
     $.ajax({
         url: "send-form.php",
         type: "POST",
         cache: false,
-        data: { "Имя": name, "Почта": email, "Телефон": tel },
+        data: { "name": name, "email": email, "tel": tel },
         dataType: "html",
-        beforeSend: ()=>{
-            $("#sendForm").prop("disable", true)
+        beforeSend: () => {
+            $("#sendForm").prop("disabled", true)
+            alert('Ваши данные отправляються на сервер. Нажмите - ОК и ожидайте...')
         },
-        success
+        success: (data) => {
+            alert("Ваши данные - доставлены! Вы получили доступ!")
+            $("#sendForm").prop("disabled", false)
+        },
+        error: function (xhr, exception) {
+            let msg = "";
+            if (xhr.status === 0) {
+                msg = "Not connect.\n Verify Network." + xhr.responseText;
+            } else if (xhr.status == 404) {
+                msg = "Requested page not found. [404]" + xhr.responseText;
+            } else if (xhr.status == 500) {
+                msg = "Internal Server Error [500]." + xhr.responseText;
+            } else if (exception === "parsererror") {
+                msg = "Requested JSON parse failed.";
+            } else if (exception === "timeout") {
+                msg = "Time out error." + xhr.responseText;
+            } else if (exception === "abort") {
+                msg = "Ajax request aborted.";
+            } else {
+                msg = "Error:" + xhr.status + " " + xhr.responseText;
+            }
+        }
     })
 })
+
+function push() {
+    console.log('пока что кнопка не работает');
+    alert('пока что кнопка не работает');
+}
--- send-form.php
+++ send-form.php
@@ -1,11 +1,13 @@
 <?php
-$file = fopen("file.txt","a+"); 
-$text="Вам пришло письмо от пользователя $name\n";
-$text="Текст сообщения:\n $email \n";
-$text="Ответить можно на адрес электронной почты $tel \n"
-flock($file, LOCK_EX);
-fwrite($file, $text);
-flock($file, LOCK_UN);
-fclose($file);
-echo "Сообщение отправленно.";
+$name = $_POST['name'];
+$email = $_POST['email'];
+$tel = $_POST['tel'];
+
+$addUser ="{Имя: '$name',\nПочта: '$email',\nТелефон: '$tel'},
+    \n";
+
+$users = fopen("leads.txt", "a");
+fputs($users, $addUser); 
+fclose($users);
+
 ?>
